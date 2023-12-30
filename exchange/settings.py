0b010f04e28fab434cf3daf9d7f7a270fdf3e8d7
abbasebadian <abbasebadiann@gmail.com>
2023-12-30 20:59:33 +0000

    allowed hosts

--- exchange/settings.py
+++ exchange/settings.py
@@ -12,14 +12,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import environ, os
 from rest_framework import authentication
-
-env = environ.Env()
-environ.Env.read_env()
-
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 
 # Quick-start development settings - unsuitable for production
@@ -31,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "82.115.18.227"]
 
 
 # Application definition
