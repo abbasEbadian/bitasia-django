3a96ed3fc53af0e23463a7e1c3f808320a9c6364
abbasebadian <abbasebadiann@gmail.com>
2024-02-11 17:14:01 +0330

    [IMP] auth: save pass

--- authentication/serializer.py
+++ authentication/serializer.py
@@ -83,6 +83,7 @@ class RegisterSerializer(serializers.Serializer):
         user = User.objects.create_user(mobile=mobile, username=mobile, first_name=first_name,
                                         last_name=last_name)
         user.set_password(password)
+        user.save()
         attrs['user'] = user
 
         return attrs
