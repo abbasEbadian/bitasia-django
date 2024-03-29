4b3c620a3d6d854b42166cb8fdfb1fc8ffea3b9e
abbasebadian <abbasebadiann@gmail.com>
2024-03-29 09:04:13 +0330

    [MOD] exchange: urls

--- exchange/urls.py
+++ exchange/urls.py
@@ -34,10 +34,10 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('api/admin/', admin.site.urls),
-    path('api/v1/', include('api.urls')),
+    path('v1/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
 
-    path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
