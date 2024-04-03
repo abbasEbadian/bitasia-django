b512cc4fcdb3555c51d5433a273a807ce0b67c16
abbasebadian <abbasebadiann@gmail.com>
2024-04-04 00:11:30 +0330

    [FIX] zarinpal: purl fix

--- zarinpal/urls.py
+++ zarinpal/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('create_rial_payment/', views.create_rial_deposit, name='create_rial_payment'),
     path('callback/', views.payment_callback, name='create_rial_payment'),
-    path('/payment/history/', views.RialDepositView.as_view(), name='payment_history')
+    path('payment/history/', views.RialDepositView.as_view(), name='payment_history')
 ]
