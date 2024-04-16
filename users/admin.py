27d47cc01aa2603b6db21edda678083867afc11d
abbasebadian <abbasebadiann@gmail.com>
2024-04-16 22:46:36 +0330

    [IMP] user: login history admin

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import CustomUser
+from .models import CustomUser, LoginHistory
 
 
 @admin.register(CustomUser)
 class CurrencyAdmin(admin.ModelAdmin):
     list_display = ("uid", "first_name", 'username')
+
+
+@admin.register(LoginHistory)
+class LoginHistoryAdmin(admin.ModelAdmin):
+    list_display = ("user_id", "successful", "ip", "create_date")
